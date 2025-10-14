41d1e44e82dc56897a4eb080dd8c956ad360e256
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-10-14 19:57:43 +0500

    graphql server is working

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,42 @@ import cors from "cors";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@as-integrations/express5";
 
-const app = express();
-app.use(cors());
+async function startGraphQLServer() {
+  const app = express();
+  const PORT = process.env.PORT || 8000;
+  // Start apollo graphQL server
+  const gqlServer = new ApolloServer({
+    // Schema
+    typeDefs: `#graphql 
+      type Query{
+        hello(name: String): String!
+        sayBapaStunning: String!
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: (_, { name }: { name: string }) => `Hor vai ${name} ki hal chal a ...`,
+        sayBapaStunning: () => "No bro mai teri tara Narak mai nai jana o murshad ne mere"
+      }
+    }
+  });
 
-app.get("/health", (req, res) => {
-  res.status(200).send("OK");
-});
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+  app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+  });
+
+
+  await gqlServer.start();
+
+  app.use("/graphql", cors(), express.json(), expressMiddleware(gqlServer));
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+startGraphQLServer()
+  .catch((error) => {
+    console.error("Failed to start the server:", error);
+    process.exit(1);
+  });
